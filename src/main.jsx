0bf63bc4a7d308186ea2cf764b736edaf57f25ef
abdallared <abdallared@users.noreply.github.com>
2026-02-12 18:24:27 +0200

    Fix routing for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/atlas-home-appliances">
+    <BrowserRouter basename="/ATLS">
       <App />
     </BrowserRouter>
   </StrictMode>,
